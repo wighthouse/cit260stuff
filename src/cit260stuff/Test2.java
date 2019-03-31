fb5d3a4b31db8f556c16e689e6cbff996ce603eb
wighthouse <wig18001@byui.edu>
2019-03-31 02:04:59 -0600

    First commit

---
+++ src/cit260stuff/Test2.java
@@ -0,0 +1,10 @@
+package cit260stuff;
+
+public class Test2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello, Maria! It worked!");
+	}
+
+}
