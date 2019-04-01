11c9e96e2f5511d950c17e1e5a8802ccf6014f6e
mczubia <38670478+mczubia@users.noreply.github.com>
2019-04-01 07:31:16 -0700

    Added a new println

--- src/cit260stuff/Test2.java
+++ src/cit260stuff/Test2.java
@@ -5,6 +5,8 @@ public class Test2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello, Maria! It worked!");
+		
+		System.out.println("Yay, you is a programmer NOW!!");
 	}
 
 }
