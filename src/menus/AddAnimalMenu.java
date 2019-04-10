0812b0f8ad8ea5efaf9d8d09802da858faa0e33d
wighthouse <wig18001@byui.edu>
2019-04-10 08:13:37 -0600

    Add animal menu

--- src/menus/AddAnimalMenu.java
+++ src/menus/AddAnimalMenu.java
@@ -1,10 +1,50 @@
 package menus;
 
+
+import cit260stuff.ShelterDatabase;
+import cit260stuff.Animal;
+import cit260stuff.Dog;
+import cit260stuff.Cat;
+
 public class AddAnimalMenu extends Menu {
 
-		//This will display the menu for adding an animal cat/dog
-	
-		// It will either call another menu to addCat or addDog or just handle both of those within this class
-	
-		// When finished it will return to the main menu
+	/**
+     * Default constructor. Does nothing.
+     */
+    public AddAnimalMenu() {
+        super();
+    }
+    
+    /**
+     * Provide the menu title (or null to skip it)
+     * @return
+     */
+    @Override
+    protected String getTitle() {
+        return "ADD AN ANIMAL";
+    }
+    
+    
+    /**
+     * Provide a menu description, or null to skip it.
+     * @return
+     */
+    @Override
+    protected String getDescription() {
+        return "Please choose the type of animal you would like to add.";
+    }
+    
+    /**
+     * Provide the list of menu items.
+     * @return
+     */
+    @Override
+    protected MenuItem[] getMenuItems() {
+        return new MenuItem[] {
+                new MenuItem('1', "Add a Dog"),
+                new MenuItem('2', "Add a Cat"),
+                new MenuItem('R', "Return to Main Menu"),
+                new MenuItem('Q', "Quit")
+        };
+    }
 }
